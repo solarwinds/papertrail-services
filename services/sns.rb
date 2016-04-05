8848ffc2c723897bcafa46cc0f9122f62d291624
Troy Davis <troy@yort.com>
2016-04-05 11:31:49 -0700

    Properly publish JSON-ified event to SNS

--- services/sns.rb
+++ services/sns.rb
@@ -14,7 +14,7 @@ class Service::SNS < Service
 
     begin
       payload[:events].each do |event|
-        topic.publish({ :default => event }.to_json)
+        topic.publish(event.to_json)
       end
     rescue AWS::SNS::Errors::AuthorizationError => e
       raise Service::ConfigurationError,
