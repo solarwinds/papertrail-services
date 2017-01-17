1262c100308ace2f54d5a6373a5af67c519ee0af
mark.martin <mark.martin@papertrail.com>
2017-01-17 12:11:38 -0800

    Ignore some transient exceptions

--- lib/papertrail_services/app.rb
+++ lib/papertrail_services/app.rb
@@ -66,13 +66,16 @@ module PapertrailServices
           Metriks.meter("papertrail_services.#{svc.hook_name}.error").mark
           Metriks.meter("papertrail_services.#{svc.hook_name}.error.email").mark
 
+          Scrolls.log_exception({ :from => :service, :saved_search_id => saved_search_id,
+            :addresses => settings[:addresses] }, e)
+
           status 400
-          report_exception(e, :saved_search_id => saved_search_id,
-            :addresses => settings[:addresses])
         rescue TimeoutError, ::PapertrailServices::Service::TimeoutError
           Metriks.meter("papertrail_services.#{svc.hook_name}.error").mark
           Metriks.meter("papertrail_services.#{svc.hook_name}.error.timeout").mark
-          # report_exception(e, :saved_search_id => payload[:saved_search][:id])
+
+          Scrolls.log_exception({ :from => :service }, e)
+
           status 500
           'error'
         rescue Object => e
