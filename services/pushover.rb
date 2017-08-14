f9bdb742847f5541ab245947e37393c7b0ab7664
Cory Duncan <cory@librato.com>
2017-08-14 09:49:13 -0700

    Fix Pushover inactivity alert (#108)

--- services/pushover.rb
+++ services/pushover.rb
@@ -1,7 +1,36 @@
 # coding: utf-8
 
 class Service::Pushover < Service
-  attr_writer :pushover
+  API_URL = "https://api.pushover.net/1/messages.json"
+
+  def post_data(body)
+    message = body[:message]
+    message = message.join(', ') if message.is_a?(Array)
+    message = message[0..1020] + "..." if message.length > 1024
+
+    if message.empty?
+      raise_config_error "Could not process payload"
+    end
+
+    data = {
+      :token => settings[:token],
+      :user => settings[:user_key],
+      :title => body[:title],
+      :message => message,
+      :timestamp => body[:timestamp].to_i,
+      :url => payload[:saved_search][:html_search_url],
+      :url_title => "View logs on Papertrail"
+    }
+    data = URI.encode_www_form(data)
+
+    resp = http_post(API_URL, data)
+
+    unless resp.success?
+      puts "pushover: #{resp.to_s}"
+
+      raise_config_error "Failed to post to Pushover"
+    end
+  end
 
   def receive_logs
     raise_config_error 'Missing pushover app token' if
@@ -10,42 +39,39 @@ class Service::Pushover < Service
       settings[:user_key].to_s.empty?
 
     events = payload[:events]
+    title  = payload[:saved_search][:name]
 
-    hosts = events.collect { |e| e[:source_name] }.sort.uniq
-    title = payload[:saved_search][:name]
-    if hosts.length < 5
-        title = "#{title} (#{hosts.join(', ')})"
+    if events.present?
+      hosts = events.collect { |e| e[:source_name] }.sort.uniq
+
+      if hosts.length < 5
+        title += " (#{hosts.join(', ')})"
       else
-        title = "#{title} (from #{hosts.length} hosts)"
-    end
+        title += " (from #{hosts.length} hosts)"
+      end
 
-    message = events.collect { |item|
-      syslog_format(item)
-    }
+      message = events.collect { |item|
+        syslog_format(item)
+      }
 
-    message = message[0..1020] + "..." if message.length > 1024
+      body = {
+        :title => title,
+        :message => message,
+        :timestamp => Time.iso8601(events[0][:received_at])
+      }
 
-    if message.empty?
-      raise_config_error "Could not process payload"
-    end
+      post_data(body)
+    else
+      frequency = frequency_phrase(payload[:frequency])
+      message   = %{0 matches found #{frequency}}
 
-    resp = pushover.notify(settings[:user_key],
-                           message,
-                           {title: title,
-                            timestamp: Time.iso8601(events[0][:received_at]).to_i,
-                            url: payload[:saved_search][:html_search_url],
-                            url_title: "View logs on Papertrail"})
+      body = {
+        :title => title,
+        :message => message,
+        :timestamp => Time.now
+      }
 
-    unless resp.ok?
-      puts "pushover: #{resp.to_s}"
-
-      raise_config_error "Failed to post to Pushover"
+      post_data(body)
     end
   end
-
-
-  def pushover
-    @pushover ||= Rushover::Client.new(settings[:token])
-  end
-
 end
--- test/pushover_test.rb
+++ test/pushover_test.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../helper', __FILE__)
 
 class PushoverTest < PapertrailServices::TestCase
-
   def test_config
     svc = service(:logs, {:token => 'a sample token'}, payload)
     assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
@@ -9,24 +8,42 @@ class PushoverTest < PapertrailServices::TestCase
     svc = service(:logs, {:user_key => 'a different token'}, payload)
     assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
   end
-  
+
   def test_logs
-    svc = service(:logs, {:token => 'a sample token',
-                          :user_key => 'a different token'},
-                  payload)
+    svc = service(:logs, {:token => 'a sample token', :user_key => 'a different token'}, payload)
 
+    body = nil
     http_stubs.post '/1/messages.json' do |env|
-      [200, {:content_type => "application/json"}, { :status => 1 }.to_json]
-
-      svc.receive_logs
+      body = CGI.parse(env[:body])
+      [200, {}, '']
     end
 
+    svc.receive_logs
 
+    assert_not_nil body
+    assert_match /Jul 22 14:10:01 alien CROND/, body['message'][0]
+    assert_equal 'cron (alien, lullaby)', body['title'][0]
+    assert_equal 'https://papertrailapp.com/searches/392', body['url'][0]
+  end
 
+  def test_no_logs
+    svc = service(:logs, {:token => 'a sample token', :user_key => 'a different token'}, payload.dup.merge(:events => []))
+
+    body = nil
+    http_stubs.post '/1/messages.json' do |env|
+      body = CGI.parse(env[:body])
+      [200, {}, '']
+    end
+
+    svc.receive_logs
+
+    assert_not_nil body
+    assert_equal '0 matches found in the past minute', body['message'][0]
+    assert_equal 'cron', body['title'][0]
+    assert_equal 'https://papertrailapp.com/searches/392', body['url'][0]
   end
 
   def service(*args)
     super Service::Pushover, *args
   end
-  
 end
